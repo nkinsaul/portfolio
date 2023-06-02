b8c90961c7150580aa6c05d80a4acdf27768797b
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-06-01 17:48:07 -0700

    Set up Components and Header folders

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from '../Header/Header';
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Natalie's Portfolio Content</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <h1>Natalie's Portfolio Site</h1>
+  )
+}
+
+export default Header;

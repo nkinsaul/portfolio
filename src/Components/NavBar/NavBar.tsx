5334b46007f0ccc42b1ba541b96d36d779670032
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-06-02 17:39:34 -0700

    Add NavBar folders and intitial setup

---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <p>Development</p>
+      <p>Audio</p>
+      <p>About</p>
+      <p>Contact</p>
+    </nav>
+  )
+}
+
+export default NavBar;

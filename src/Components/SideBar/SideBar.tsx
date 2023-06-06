6376b32a8c7201035da6ac181f809168c6f0bf27
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-06-05 17:16:43 -0700

    Add a black background to the SideBar

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './SideBar.css'
 
 const SideBar = () => {
   return (
     <div className="side-bar">
-      <h1>Projects</h1>
+     <h1>Projects</h1>
     </div>
   )
 }

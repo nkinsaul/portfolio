b353f8ea21864e9c04a0b8dc32a995ea8f9e6bdf
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-06-06 17:31:51 -0700

    Install Router and set up routes for development projects and audio projects

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import SideBar from '../SideBar/SideBar';
+import Landing from '../Landing/Landing';
+import Audio from '../Audio/Audio';
+import { Route, Routes } from 'react-router-dom';
 
 
 const App = () => {
@@ -10,7 +13,10 @@ const App = () => {
       <Header />
       <main>
         <SideBar />
-        <h1>Natalie's Portfolio Content</h1>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/audio" element={<Audio />} />
+        </Routes>
       </main>
     </body>
   )
---
+++ src/Components/Audio/Audio.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Audio = () => {
+  return (
+    <h1>Audio Projects</h1>
+  )
+}
+
+export default Audio

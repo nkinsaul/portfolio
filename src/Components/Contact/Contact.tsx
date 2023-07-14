233c8e0862668e86ca4c45097bcd3d192d29d0fd
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-07-14 12:54:18 -0700

    Create controlled form for Contact page

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,8 +1,42 @@
-import React from "react";
+import { useState } from "react";
+import "./Contact.css"
 
 const Contact = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
-    <h1>Contact</h1>
+    <form className="form">
+      <h1>Please Reach Out!</h1>
+      <div className="form-inputs">
+        <input 
+          className="name"
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <input 
+          className="email"
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input 
+          className="message"
+          type="text"
+          name="message"
+          placeholder="Message"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+        />
+      </div>
+    </form>
   )
 }
 

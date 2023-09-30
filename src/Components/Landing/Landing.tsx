47a80abff06f2bcf21d9b053c772615503e93567
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-09-30 15:29:13 -0700

    Add first project image

--- src/Components/Landing/Landing.tsx
+++ src/Components/Landing/Landing.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import './Landing.css'
 import SideBar from "../SideBar/SideBar";
+import listenUp1 from "../../images/ss_1.png"
 
 const Landing = () => {
+
   return (
     <div className="landing">
       <SideBar />
-      <h1>Development Projects</h1>
+      <div className="main-content">
+        <h1>Development Projects</h1>
+        <img src={listenUp1} />
+      </div>
     </div>
   )
 }

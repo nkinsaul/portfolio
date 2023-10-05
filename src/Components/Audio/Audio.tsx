e59d38f0a8617abe4632ebd43704c857119c2328
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-10-05 15:47:14 -0700

    Create new landing and project section

--- src/Components/Audio/Audio.tsx
+++ src/Components/Audio/Audio.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import projects from "../../projects/projects.json"
 
 const Audio = () => {
+
+  const myProjects = (projects: {name: string, description: string, technologies: string}[]) => {
+    return projects.map((project, index) =>  (
+        <div key={index}>
+          <h1>{project.name}</h1>
+          <p>{project.description}</p>
+          <p>{project.technologies}</p>
+        </div>
+      )
+    )
+  }
+
   return (
-    <h1>Audio Projects</h1>
+    <div>
+      <h1>Projects</h1>
+      <div>
+        {myProjects(projects.projects)}
+      </div>
+        
+    </div>
   )
 }
 

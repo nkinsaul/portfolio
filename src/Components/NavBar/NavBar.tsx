04cb3d0f229c1e1af5cdfa7e8ebd957796a8a1a7
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-10-14 17:59:13 -0700

    Fix borders make navBar solid

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <Link to="/">HOME</Link>
       <Link to="/projects">PROJECTS</Link>
       <Link to="/about">ABOUT</Link>
       <Link to="/contact">CONTACT</Link>

281c6e858e4a890e09cfa136db966928992d3bfe
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-10-21 20:10:40 -0700

    Fix landing page styling

--- src/Components/Landing/Landing.tsx
+++ src/Components/Landing/Landing.tsx
@@ -5,7 +5,7 @@ const Landing = () => {
 
     return (
       <div className="intro">
-        <h1>WELCOME!  MY NAME IS <span className="name">NATALIE KINSAUL</span>AND I AM A FULL-STACK WEB DEVELOPER. </h1>
+        <h1>WELCOME!  MY NAME IS <span className="my-name">NATALIE KINSAUL</span> AND I AM A FULL-STACK WEB DEVELOPER. </h1>
       </div>
     )
 
